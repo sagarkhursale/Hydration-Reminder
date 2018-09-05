46d028a6c355e0d1bd07082398b6bb7b991b7eb9
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-05 17:13:14 +0530

    MainActivity ui done.

--- app/src/main/java/com/sagar/hydrationreminder/MainActivity.java
+++ app/src/main/java/com/sagar/hydrationreminder/MainActivity.java
@@ -2,14 +2,37 @@ package com.sagar.hydrationreminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // Views
+    private TextView mWaterCountDisplay;
+    private TextView mChargingCountDisplay;
+    private ImageView mChargingImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWaterCountDisplay = findViewById(R.id.tv_water_count);
+        mChargingCountDisplay = findViewById(R.id.tv_charging_reminder_count);
+        mChargingImageView = findViewById(R.id.iv_power_increment);
+
+
+
+        // end
+    }
+
+
+
+    // Image Button -Water_Increment..
+    public void incrementWater(View view) {
     }
 
 
