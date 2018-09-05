d72e53d3381a55bbb899f0bb724966ab48492146
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-05 18:38:48 +0530

    remindUserBecozCharging( ) that creates notification to drink water.

--- app/src/main/java/com/sagar/hydrationreminder/utilities/NotificationUtil.java
+++ app/src/main/java/com/sagar/hydrationreminder/utilities/NotificationUtil.java
@@ -1,10 +1,21 @@
 package com.sagar.hydrationreminder.utilities;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.sagar.hydrationreminder.MainActivity;
+import com.sagar.hydrationreminder.R;
+
 
 /**
  * Utility class for creating hydration notifications
@@ -12,6 +23,8 @@ import com.sagar.hydrationreminder.MainActivity;
 public class NotificationUtil {
 
     private static final int WATER_REMINDER_PENDING_INTENT_ID = 3417;
+    private static final int WATER_REMINDER_NOTIFICATION_ID = 1138;
+    private static final String WATER_REMINDER_NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
 
 
     private static PendingIntent contentIntent(Context context) {
@@ -21,6 +34,45 @@ public class NotificationUtil {
                 WATER_REMINDER_PENDING_INTENT_ID,
                 startActivityIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+
+    private static Bitmap largeIcon(Context context) {
+        Resources res = context.getResources();
+        Bitmap largeIcon = BitmapFactory.decodeResource(res, R.drawable.ic_local_drink_black_24px);
+        return largeIcon;
+    }
+
+
+    public static void remindUserBecauseCharging(Context context) {
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(
+                    WATER_REMINDER_NOTIFICATION_CHANNEL_ID,
+                    context.getString(R.string.main_notification_channel_name),
+                    NotificationManager.IMPORTANCE_HIGH);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(mChannel);
+        }
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context,WATER_REMINDER_NOTIFICATION_CHANNEL_ID)
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_drink_notification)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle(context.getString(R.string.charging_reminder_notification_title))
+                .setContentText(context.getString(R.string.charging_reminder_notification_body))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(
+                        context.getString(R.string.charging_reminder_notification_body)))
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(contentIntent(context))
+                .setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        }
+        assert notificationManager != null;
+        notificationManager.notify(WATER_REMINDER_NOTIFICATION_ID, notificationBuilder.build());
     }
 
     // END
