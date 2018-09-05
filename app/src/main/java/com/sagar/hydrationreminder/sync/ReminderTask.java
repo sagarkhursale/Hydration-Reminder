e654b9d0a285ceb563000cc5816148fd2adf72e3
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-05 18:50:52 +0530

    update ReminderTask

--- app/src/main/java/com/sagar/hydrationreminder/sync/ReminderTask.java
+++ app/src/main/java/com/sagar/hydrationreminder/sync/ReminderTask.java
@@ -1,23 +1,29 @@
 package com.sagar.hydrationreminder.sync;
 
 import android.content.Context;
+
+import com.sagar.hydrationreminder.utilities.NotificationUtil;
 import com.sagar.hydrationreminder.utilities.PreferenceUtilities;
 
 
 public  class ReminderTask {
 
     public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+    public static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
 
 
     public static void executeTask(Context context, String action) {
         if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
             incrementWaterCount(context);
+        }else if (ACTION_DISMISS_NOTIFICATION.equals(action)) {
+            NotificationUtil.clearAllNotifications(context);
         }
     }
 
 
     private static void incrementWaterCount(Context context) {
         PreferenceUtilities.incrementWaterCount(context);
+        NotificationUtil.clearAllNotifications(context);
     }
 
 
