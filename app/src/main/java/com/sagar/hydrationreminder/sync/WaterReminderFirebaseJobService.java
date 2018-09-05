7352a1eae1d5e25b5ef13626bd3838f3225f422b
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-05 20:06:54 +0530

    run charging reminder task off of the main thread.

--- app/src/main/java/com/sagar/hydrationreminder/sync/WaterReminderFirebaseJobService.java
+++ app/src/main/java/com/sagar/hydrationreminder/sync/WaterReminderFirebaseJobService.java
@@ -1,19 +1,43 @@
 package com.sagar.hydrationreminder.sync;
 
+import android.annotation.SuppressLint;
 import android.app.job.JobParameters;
+import android.content.Context;
+import android.os.AsyncTask;
+
 import com.firebase.jobdispatcher.JobService;
 
 
 public class WaterReminderFirebaseJobService extends JobService {
 
+    private AsyncTask mBackgroundTask;
+
+    @SuppressLint("StaticFieldLeak")
     @Override
-    public boolean onStartJob(com.firebase.jobdispatcher.JobParameters job) {
-        return false;
+    public boolean onStartJob(final com.firebase.jobdispatcher.JobParameters jobParameters) {
+        mBackgroundTask = new AsyncTask() {
+
+            @Override
+            protected Object doInBackground(Object[] params) {
+                Context context = WaterReminderFirebaseJobService.this;
+                ReminderTask.executeTask(context, ReminderTask.ACTION_CHARGING_REMINDER);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(jobParameters, false);
+            }
+        };
+
+        mBackgroundTask.execute();
+        return true;
     }
 
     @Override
     public boolean onStopJob(com.firebase.jobdispatcher.JobParameters job) {
-        return false;
+        if (mBackgroundTask != null) mBackgroundTask.cancel(true);
+        return true;
     }
 
 
